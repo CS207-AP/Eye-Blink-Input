2091d6448a3a54a25039610cf3ab4fe49e4593e8
SahilAggarwal124 <sahil.aggarwal_ug19@ashoka.edu.in>
2018-12-07 01:11:32 +0530

    Update dsd.py

--- dsd.py
+++ dsd.py
@@ -63,7 +63,7 @@ class obj:
         for a1, a2 in zip(arr1, arr2):
             toret = toret + abs(a1-a2)    
         return toret
-
+    
     def detect1(self, img):
         fet = self.find_feature(img)
         if(type(fet)!=type(None)):
@@ -83,7 +83,7 @@ class obj:
             return ("Status", self.possible_states[diffs.index(min(diffs))])
         else:
             return ("Error", "Object Not Found")
-
+        
     def detect2(self, img):
         fet = self.find_feature(img)
         if(type(fet)!=type(None)):
@@ -104,7 +104,6 @@ class obj:
         else:
             return ("Error", "Object Not Found")
         
-
     def initialize(self, comm_method):
         cap = cv2.VideoCapture(0)
         for state in self.possible_states:
@@ -122,3 +121,27 @@ class obj:
             comm_method(state+" initizalized.")
             time.sleep(2)
         return cap
+
+eye = obj("Eyes",["Open", "Close"],[[0.07,0.3,0.05,0.6],[0.7,0.93,0.05,0.6]],'/home/k00lk0der1/opencv/data/haarcascades/haarcascade_frontalface_default.xml','/home/k00lk0der1/opencv/data/haarcascades/haarcascade_mcs_eyepair_big.xml')
+cap = eye.initialize(espeak)
+eye.initialize_means_roi()
+font = cv2.FONT_HERSHEY_SIMPLEX
+send=False
+while(True):
+    ret, img = cap.read()
+    ty, st = eye.detect2(img)
+    if(send):
+        s.send((st+"\n").encode('utf-8'))
+    cv2.putText(img, st, (20,40), font, 1,(255,255,255),2)
+    cv2.imshow('imgSate', img)
+    if 0xFF & cv2.waitKey(5) == 27:
+        break
+    if cv2.waitKey(5) == 32:
+        if(not send):
+            print("Connect")
+            s = socket.socket()
+            s.connect(("10.1.28.125", 8960))#Change IP here
+            send=True
+    
+cv2.destroyAllWindows()
+cap.release()
